285d239294b3dd64afce41621385f9b5c245e92a
JasonYates75 <jasonyates75@gmail.com>
2014-03-22 21:59:58 +1100

    added completed folders to finish website

--- contact.php
+++ contact.php
@@ -16,6 +16,7 @@
   
 <!--Header-->
 <header id="header">
+<!--Logo, Social links, phone and support-->
 <div class="row">
   <!--Logo Image and text-->
     <div class="small-10 small-centered large-3 large-uncentered columns" id="logo-image">
@@ -27,22 +28,29 @@
     </div> 
     <!--End Logo Image and Text-->
     
-    <!--Social links and Phone-->
+    <!--Social links, Phone and Support-->
  	<div class="large-3 columns" id="social_large_columns">
       <ul class="small-block-grid-4" id="social_small_grid_3">
               <li><a href="http://www.jasonyates75.com.au"><i class="fi-social-facebook size-48"></i></a></li>
               <li><a href="#"><i class="fi-social-twitter size-36" ></i></a></li>
-              
               <li><a href="tel:1300766933"><i class="fi-mobile size-36" ></i></a></li>
       </ul>
-      <ul class="small-12 columns" id="mobile">
-              
-            <a href="tel:1300766933"><p>Phone 1300 79 69 33</p></a>
-              
-      </ul>
+      
+      
+      <div class="small-12 large-12 columns" id="mobile">
+      	<a href="tel:1300766933"><p>Phone 1300 79 69 33</p></a>
+      </div>
+      
+      
+      
+      <div class="small-12 large-12 columns">
+		<a href="http://aus.myconnectwise.net/integrateit" class="button right">Service Desk Client Login</a><br>
+      </div>
+        
     </div>
 </div>
-<!--End Social links and phone-->
+<!--End Logo, Social links, phone and support-->
+ 
  
  
     
@@ -50,39 +58,47 @@
 <!--Nav-->
 <div id="nav">
     <div class="row">
-    <div class="contain-to-grid sticky">
-    <nav class="top-bar" data-topbar>
-    <ul class="title-area">
-        <li class="name" style="background-color:#049EE8">
-        	<h1><a href="#">IntegrateIT</a></h1>
-        </li>
-        <li class="toggle-topbar menu-icon"><a href="#">Menu</a></li>
-    </ul>
+    	<div class="contain-to-grid sticky">
+            <nav class="top-bar" data-topbar>
+                <ul class="title-area">
+                    <li class="name" style="background-color:#049EE8">
+                        <h1><a href="#">IntegrateIT</a></h1>
+                    </li>
+                    <li class="toggle-topbar menu-icon"><a href="#">Menu</a></li>
+                </ul>
+            
+            <!--<div class="large-12 columns">-->
+                <section class="top-bar-section">
+                <!-- Left Nav Section -->
+                    <ul class="left">
+                        <li><a href="index.html" >Home</a></li>
+                        <li><a href="about.html">About</a></li>
+                        <li class="has-dropdown">
+                            <a href="services.html">Services</a>
+                                <ul class="dropdown">
+                                <li><a href="professional_it.html">Professional IT Services</a></li>
+                                <li><a href="managed_services.html">Managed Services </a></li>
+                                <li><a href="it_consulting_audit_services.html">IT Consulting / Audit Services</a></li>
+                              </ul>
+                        </li>
+                        <li class="has-dropdown">
+                            <a href="solutions.html">Solutions</a>
+                            <ul class="dropdown">
+                                <li><a href="cloud_computing.html">Cloud Computing</a></li>
+                               <li><a href="disaster_recovery_business_continuity.html">Disaster Recovery & Business Continuity</a></li>
+                               <li><a href="wireless_solutions.html">Wireless Solutions</a></li>
+                            </ul>
+                        </li>
+                    <li><a href="support.html">Support</a></li>
+                    <li><a href="contact.php" class="selected">Contact Us</a></li>
+                    <li><a href="vendors.html">Vendors</a></li>
+                    </ul>
+                
+                
+                </section>
+            </nav>
+    	</div>
     
-    <!--<div class="large-12 columns">-->
-    <section class="top-bar-section">
-    <!-- Left Nav Section -->
-    <ul class="left">
-    <li><a href="index.html">Home</a></li>
-    <li><a href="about.html">About</a></li>
-    <li><a href="services.html" data-dropdown="drop1">Services</a>
-        <ul id="drop1" class="f-dropdown" data-dropdown-content>
-        <li><a href="#">Professional IT Services</a></li>
-        <li><a href="#">Managed Services </a></li>
-        <li><a href="#">Disaster recovery & Business Continuity</a></li>
-        <li><a href="#">IT Consulting / Audit Services</a></li>
-        <li><a href="#">Cloud Computing</a></li>
-        <li><a href="#">Wireless Solutions</a></li>
-        </ul>
-    </li>
-    <li><a href="contact.html" id="selected">Contact Us</a></li>
-    <li><a href="#">Vendors</a></li>
-    </ul>
-    
-    
-    </section>
-    </nav>
-    </div>
     </div>
 </div>
 <!-- End Nav -->
@@ -96,7 +112,7 @@
     <!-- Contact Header -->
     <div class="row">
         <div class=" small-12 large-12 columns">
-            <h3>Contact Us</h3>
+            <h2>Contact Us</h2>
             <p>Please complete the form below and one of our staff members will get in contact with you shortly.
             </p> 
          </div>
@@ -132,7 +148,7 @@
 	    echo '<p>Something went wrong, go back and try again!</p>'; 
 	} 
     } else if ($_POST['submit'] && $human != '4') {
-	echo '<p>You answered the anti-spam question incorrectly!</p>';
+	echo '<p>You answered the question incorrectly!</p>';
     }
 ?>
                
@@ -169,16 +185,16 @@
                 <div class="large-2 columns">    
                 <label class="inline">Message</label>
                 </div>
-                <div class="large-10 columns">
-                <textarea name="message" placeholder="Leave a message"></textarea>
+                 <div class="large-10 columns"> 
+                <textarea name="message" cols="10" placeholder="Leave a message"></textarea>
                 </div>
                 </div>
                 
                 <div class="row collapse">
-                <div class="large-8 columns"> 
-                <label class="inline">What is 2+2? (Anti Spam)</label>	
+                <div class="large-2 columns"> 
+                <label class="inline">What is 2+2?</label>	
                 </div>
-                <div class="large-4 columns">
+                <div class="large-10 columns">
                 <input name="human" type="text" placeholder="Your Answer">
                 </div>
                 </div>
@@ -202,17 +218,17 @@
           	<a href="https://www.google.com.au/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=5%2F209+Days+Rd,+Grange+QLD+4051&amp;aq=&amp;sll=-25.335448,135.745076&amp;sspn=65.357904,71.279297&amp;ie=UTF8&amp;hq=&amp;hnear=5%2F209+Days+Rd,+Grange+Queensland+4051&amp;t=m&amp;z=14&amp;ll=-27.425642,153.014035" style="color:#0000FF;text-align:left"><p style="color: white">View Larger Map</p></a>
           </small>
             
-            <h5>Address:</h5>
-            <p style="color: white">5/209 Days Rd,
+            <h5><span class="integrate-blue">Address:</span></h5>
+            <p id="address">5/209 Days Rd,
              Grange QLD 4051
             </p>
             
-            <h5>Postal:</h5>
-            <p style="color: white">PO BOX 5281 <br/>
-									   Brendale DC QLD 4500</p>
+            <h5><span class="integrate-blue">Postal:</span></h5>
+            <p id="postal">PO BOX 5281 <br/>
+				Brendale DC QLD 4500</p>
             
             
-            <p style="color: white">PH: 1300 79 69 33<br/>
+            <p>PH: 1300 79 69 33<br/>
                 FAX: 07 3882 2329
             </p>
        </div>
@@ -231,14 +247,18 @@
                     <div id="footer_content">
                         <div class="large-6 columns">
                         <p>© IntegrateIT Australia</p>
+                        <p style="font-size:12px">Web Design by <a href="www.jasonyates75.com.au">JasonYates75 Web Designs</a></p>
                         </div>
                         <div class="large-6 columns">
-                            <ul class="inline-list right">
-                                <li><a href="#">Home</a></li>
-                                <li><a href="#">About Us</a></li>
-                                <li><a href="#">Services</a></li>
-                                <li><a href="#">Contact Us</a></li>
-                                <li><a href="#">Vendors</a></li>
+                            <ul class="inline-list right" id="footer-menu-list">
+                                <li><a href="index.html">Home</a></li>
+                                <li><a href="about.html">About Us</a></li>
+                                <li><a href="services.html">Services</a></li>
+                                <li><a href="solutions.html">Solutions</a></li>
+                                <li><a href="support.html">Support</a></li>
+                                <li><a href="contact.php"  class="selected">Contact Us</a></li>
+                                <li><a href="vendors.html">Vendors</a></li>
+                                <li><a href="terms_conditions.html">Terms of Use & Conditions</a></li>
                             </ul>
                         </div>
                         </div>
